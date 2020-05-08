646a0545e22a1f20b56a94d86434eb596d497c6f
Héctor Espinoza <hespinoza@uchile.cl>
2020-05-08 17:18:44 -0400

    changed questios to json file

--- end.js
+++ end.js
@@ -5,13 +5,32 @@ const finalScore = document.getElementById('final-score');
 // Here we access local storage set in game.js
 const mostRecentScore = localStorage.getItem('mostRecentScore');
 
+
+// if item is empty, it's just and empty array
+const highScores = localStorage.getItem('highScores') ? JSON.parse(localStorage.getItem("highScores")) : [];
+
+const MAX_HIGH_SCORES = 5;
+
 finalScore.innerText = mostRecentScore;
 
 username.addEventListener('keyup', () => {
     saveScoreBtn.disabled = !username.value;
 })
 
+// here is the function that saves the score
 saveScoreBtn.addEventListener('click', (e) => {
-    console.log('clicked the save button!');
     e.preventDefault();
+
+    const score = {
+        score: mostRecentScore,
+        name: username.value
+    };
+    highScores.push(score);
+    highScores.sort((a, b) => b.score - a.score);
+    highScores.splice(MAX_HIGH_SCORES);
+
+    localStorage.setItem('highScores', JSON.stringify(highScores));
+    window.location.assign('/');
+    console.log(highScores);
+
 });
--- game.js
+++ game.js
@@ -11,33 +11,19 @@ let score = 0;
 let questionCounter = 0;
 let availableQuestions = [];
 
-let questions = [
-    {
-        question: "Inside which HTML element do we put the JavaScript?",
-        choice1: "<script>",
-        choice2: "<javascript>",
-        choice3: "<js>",
-        choice4: "<scripting>",
-        answer: 1
-    },
-    {
-        question:
-            "What is the correct syntax for referring to an external script called 'xxx.js'?",
-        choice1: "<script href='xxx.js'>",
-        choice2: "<script name='xxx.js'>",
-        choice3: "<script src='xxx.js'>",
-        choice4: "<script file='xxx.js'>",
-        answer: 3
-    },
-    {
-        question: " How do you write 'Hello World' in an alert box?",
-        choice1: "msgBox('Hello World');",
-        choice2: "alertBox('Hello World');",
-        choice3: "msg('Hello World');",
-        choice4: "alert('Hello World');",
-        answer: 4
-    }
-];
+let questions = [];
+
+// fetching questions from json file, game fucntion only works after promise's response.
+fetch("questions.json").then(res => {
+    console.log(res);
+    return res.json();
+}).then(loadedQuestions => {
+    console.log(loadedQuestions);
+    questions = loadedQuestions;
+    startGame();
+}).catch(error => {
+    alert(error);
+});
 
 //CONSTANTS
 const CORRECT_BONUS = 10;
@@ -47,7 +33,6 @@ const startGame = () => {
     questionCounter = 0;
     score = 0;
     availableQuestions = [...questions];
-    console.log(availableQuestions);
     getNewQuestion();
 }
 
@@ -55,7 +40,6 @@ const getNewQuestion = () => {
     if (availableQuestions.length === 0 || questionCounter >= MAX_QUESTIONS) {
         //When there is no more questions we go to the end page and we store the score in localStorage
         localStorage.setItem('mostRecentScore', score);
-        
         return window.location.assign('/end.html');
     }
     questionCounter++;
@@ -113,4 +97,3 @@ const incrementScore = num => {
     score += num;
     scoreText.innerText = score;
 }
-startGame();
