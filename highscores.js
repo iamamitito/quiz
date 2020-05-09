c7b7e23d46efb5f05670eb9dae75102632d0f797
Héctor Espinoza <hespinoza@uchile.cl>
2020-05-09 01:50:55 -0400

    styles added

--- highscores.js
+++ highscores.js
@@ -3,6 +3,6 @@ const highScoresList = document.getElementById('high-scores-list');
 const highScores = localStorage.getItem('highScores') ? JSON.parse(localStorage.getItem("highScores")) : [];
 
 highScoresList.innerHTML =  highScores.map(score => {
-    return `<li class="high-score"> ${score.name} - ${score.score}</li>`
+    return `<li class="high-score">${score.name} <span class="high-score-marker">${score.score}</span></li>`
 })
 .join('');
